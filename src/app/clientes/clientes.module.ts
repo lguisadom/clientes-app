b062e688f31c6be01f399c9e6b3cfb9e3a9914e4
Luis Guisado <lguisadom@gmail.com>
2021-06-23 20:36:54 -0500

    Creación de componente AltaCliente

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientesService } from './clientes.service';
+import { AltaClienteComponent } from './alta-cliente/alta-cliente.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AltaClienteComponent
+  ],
   imports: [
     CommonModule
   ],
