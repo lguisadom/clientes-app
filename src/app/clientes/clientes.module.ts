771d03e52d0be8cefac6b49d4baf9da58ab450c9
Luis Guisado <lguisadom@gmail.com>
2021-06-23 20:29:05 -0500

    Creación de servicio ClientesService

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClientesService } from './clientes.service';
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    ClientesService
   ]
 })
 export class ClientesModule { }
