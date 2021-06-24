6282e62e8776914d8607607145f70361a10bc010
Luis Guisado <lguisadom@gmail.com>
2021-06-24 00:07:48 -0500

    Componente listado de clientes

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ClientesService } from './clientes.service';
 import { AltaClienteComponent } from './alta-cliente/alta-cliente.component';
 import { FormsModule } from '@angular/forms';
+import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
 
 
 @NgModule({
   declarations: [
-    AltaClienteComponent
+    AltaClienteComponent,
+    ListadoClientesComponent
   ],
   imports: [
     CommonModule,
@@ -17,7 +19,8 @@ import { FormsModule } from '@angular/forms';
     ClientesService
   ],
   exports: [
-    AltaClienteComponent
+    AltaClienteComponent,
+    ListadoClientesComponent
   ]
 })
 export class ClientesModule { }
