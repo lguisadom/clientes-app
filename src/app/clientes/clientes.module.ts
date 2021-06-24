ace26c9426144581c194accf661bd897cb00dc25
Luis Guisado <lguisadom@gmail.com>
2021-06-23 20:55:48 -0500

    Importacion de FormsModule para usar ngModel

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientesService } from './clientes.service';
 import { AltaClienteComponent } from './alta-cliente/alta-cliente.component';
-
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +10,8 @@ import { AltaClienteComponent } from './alta-cliente/alta-cliente.component';
     AltaClienteComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   providers: [
     ClientesService
